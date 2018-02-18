ae7f18edf8ad469e8c1314335fea095d89c104f2
Anil Vishwakarma <vishwakarmaanil321@gmail.com>
2018-02-18 12:50:29 -0600

    Com

--- Coding Challenges/src/RotateMatrix.java
+++ Coding Challenges/src/RotateMatrix.java
@@ -4,15 +4,14 @@ public class RotateMatrix
 	public static void main(String args[])
 	{
 		int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
-		
-		int [][] rotate = rotate(matrix);
-		
-	
+
+		int[][] rotate = rotate(matrix);
+
 		for (int i = 0; i < 4; i++)
 		{
 			for (int j = 0; j < 4; j++)
 			{
-				System.out.print( rotate[i][j] + " ");
+				System.out.print(rotate[i][j] + " ");
 			}
 			System.out.println();
 			System.out.println();
@@ -32,14 +31,16 @@ public class RotateMatrix
 			{
 
 				int offset = i - first;
+
+				int top = matrix[first][i];
+
+				matrix[first][i] = matrix[last - offset][first];
 				
-				int top  = matrix [last-offset][first];
-				
-				matrix [last-offset][first] = matrix [last][last-offset];
-				
-				matrix [last][last-offset] = matrix [i][last];
-				
-				matrix [i][last] = top;
+				matrix[last - offset][first] = matrix[last][last - offset];
+
+				matrix[last][last - offset] = matrix[i][last];
+
+				matrix[i][last] = top;
 			}
 
 		}
